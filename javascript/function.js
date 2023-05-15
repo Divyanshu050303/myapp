1d137dbbfc86fc245d8346d0e03292874a469d4a
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2023-05-15 06:15:19 +0530

    function in javascript

---
+++ javascript/function.js
@@ -0,0 +1,30 @@
+// Normal funcion 
+function normal(name) {
+     console.log(name);
+}
+normal("Divyanshu")
+
+//Arrow functions
+const ArrowFunction=(name)=>{
+    console.log(name);
+}
+ArrowFunction("Divyanshu")
+
+// If we have one argument in the arrow function then we rempmove the pranthises 
+
+const ArrowFunction1=name=>{
+    console.log(name);
+}
+ArrowFunction1("Singh")
+
+// return statement with arrow function
+const multiply=(number)=>{
+    return number*3;
+}
+console.log(multiply(4));
+
+// if we have only one line to return then we use 
+const add=(number1, number2)=> number1+number2;
+console.log(add(4,6));
+    
+ 
