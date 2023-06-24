75fe4909cdf2100c05b503b8d18a52389c5e322c
divyanshu050303 <divyanshusingh7060@gmail.com>
2023-06-24 22:00:06 +0530

    header of food delevery app

--- foodorder/src/App.js
+++ foodorder/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './component/layout/Header';
 function App() {
   return (
     <div className="App">
-       Food Delivery App
+       <Header/>
     </div>
   );
 }
