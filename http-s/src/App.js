f42a28413161f301e2af7f80c3df068a78e217c6
divyanshu050303 <divyanshusingh7060@gmail.com>
2023-06-30 22:44:08 +0530

    update api

--- http-s/src/App.js
+++ http-s/src/App.js
@@ -23,14 +23,14 @@ function App() {
   // }
   async function fetchMoviesHandler(){
     setIsLoading(true)
-    const response=await fetch('https://swapi.dev/api/films/')
+    const response=await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=27bf5569ec504c39a70da5ae163bb872')
     const data=await response.json();
-    const transformedMovies =data.results.map((moviesData)=>{
+    const transformedMovies =data.articles.map((moviesData)=>{
       return {
-        id:moviesData.episode_id,
+        id:moviesData.author,
           title:moviesData.title,
-          openingText:moviesData.opening_crawl,
-          releaseDate:moviesData.release_date
+          openingText:moviesData.discription,
+          releaseDate:moviesData.url
       }
     })
     SetMovies(transformedMovies)
--- http-s/src/components/Movie.js
+++ http-s/src/components/Movie.js
@@ -5,8 +5,8 @@ import classes from './Movie.module.css';
 const Movie = (props) => {
   return (
     <li className={classes.movie}>
-      <h2>{props.title}</h2>
-      <h3>{props.releaseDate}</h3>
+      <h3>{props.title}</h3>
+      <h4>{props.releaseDate}</h4>
       <p>{props.openingText}</p>
     </li>
   );
