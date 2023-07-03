7aeac430b9903765bff0b7600ce9ef379e75e56b
divyanshu050303 <divyanshusingh7060@gmail.com>
2023-07-03 08:52:27 +0530

    https

--- http-s/src/App.js
+++ http-s/src/App.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie'
 
 function App() {
   const [movies, SetMovies]=useState([]);
   const [isLoading,setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
   // function fetchMoviesHandler() {
   //   fetch('https://swapi.dev/api/films/').then((response)=>{
   //     return response.json();
@@ -23,8 +25,13 @@ function App() {
   // }
   async function fetchMoviesHandler(){
     setIsLoading(true)
+    setError(null);
+    try{
     const response=await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=27bf5569ec504c39a70da5ae163bb872')
     const data=await response.json();
+    if(!response.ok){
+      throw new Error("Something went wrong here")
+    }
     const transformedMovies =data.articles.map((moviesData)=>{
       return {
         id:moviesData.author,
@@ -34,16 +41,27 @@ function App() {
       }
     })
     SetMovies(transformedMovies)
-    setIsLoading(false)
+    
+  } catch(error){
+    setError(error.message)
+  }
+  setIsLoading(false)
+  }
+  function addMovieHandler(movie){
+    console.log(movie.title)
   }
   return (
     <React.Fragment>
+    <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
         {!isLoading && <MoviesList movies={movies} />}
         {isLoading && <p>Loading...</p> }
+        {!isLoading && error &&<p>Loading...</p>}
       </section>
     </React.Fragment>
   );
